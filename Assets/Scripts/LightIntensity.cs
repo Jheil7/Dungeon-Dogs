892a0ed2972bdfd167a98b16066d10e79a131bae
Jheil7 <grcsoccer7.jh@gmail.com>
2023-10-18 19:56:25 -0400

    added light meter and light dimming

---
+++ Assets/Scripts/LightIntensity.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using FunkyCode;
+using FunkyCode.LightingSettings;
+using UnityEngine;
+
+public class LightIntensity : MonoBehaviour
+{
+    LightControl lightControl;
+    Light2D light2D;
+    [SerializeField] float startingLightSize;
+    float lightSize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lightControl=FindAnyObjectByType<LightControl>();
+        light2D=GetComponent<Light2D>();
+        lightSize=startingLightSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        light2D.size=lightSize;
+        lightSize=startingLightSize*(lightControl.LightValue()/lightControl.MaxLightValue());
+    }
+}
