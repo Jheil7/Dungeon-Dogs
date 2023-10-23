e204a87705cc34db459479cbf24fe585c1dba0cb
Bert <bretsmith1124@gmail.com>
2023-10-22 21:39:47 -0500

    added title sprite

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -2,10 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] Image titleSpriteOn;
+    [SerializeField] Image titleSpriteOff;
+    
+    public void Start() {
+        titleSpriteOff.enabled = false;
+    }
+    
     public void NewGame(){
+        StartCoroutine(NewGameRoutine());
+    }
+
+    IEnumerator NewGameRoutine() {
+        titleSpriteOn.enabled = false;
+        titleSpriteOff.enabled = true;
+        yield return new WaitForSecondsRealtime(3);
         SceneManager.LoadScene(1);
     }
 
